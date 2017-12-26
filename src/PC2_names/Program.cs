4602ac6482acf45b90f95d9acd6157495e7ccb56
peach <skpikao233@gmail.com>
2017-12-26 21:53:31 +0000

    Add source and sample

---
+++ src/PC2_names/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC2_names
+{
+    class Program
+    {
+        private static void ReadNameFromOffset(string filePath, int offsetSize = 25)
+        {
+            try
+            {
+                char[] allowedChars = Enumerable.Range('A', 26).Select(x => (char)x).ToArray();
+                string returnSTR = "";
+                byte[] offsetRead = new byte[offsetSize];
+                using (BinaryReader reader = new BinaryReader(new FileStream(filePath, FileMode.Open)))
+                {
+                    reader.BaseStream.Seek(8, SeekOrigin.Begin);
+                    reader.Read(offsetRead, 0, offsetSize);
+                }
+                string byteSTR = Encoding.Default.GetString(offsetRead);
+
+                foreach (char offsetChar in byteSTR)
+                {
+                    if (allowedChars.Contains(offsetChar) || offsetChar.ToString() == "_")
+                    {
+                        returnSTR += offsetChar;
+                    }
+                }
+                Console.WriteLine(filePath + "     returned    " + returnSTR + " /n" + Environment.NewLine);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = " /n marks the end of the name";
+            if (args.Length > 0)
+            {
+                Console.WriteLine(Environment.NewLine + "Enter custom offset size / return for default");
+                string customSize = Console.ReadLine();
+                FileAttributes attr = File.GetAttributes(args[0]);
+                if (customSize == "")
+                {
+                    if (attr.HasFlag(FileAttributes.Directory))
+                    {
+                        foreach (string meb in Directory.GetFiles(args[0]))
+                        {
+                            ReadNameFromOffset(meb);
+                        }
+                    }
+                    else
+                    {
+                        ReadNameFromOffset(args[0]);
+                    }
+                }
+                else
+                {
+                    if (attr.HasFlag(FileAttributes.Directory))
+                    {
+                        foreach (string meb in Directory.GetFiles(args[0]))
+                        {
+                            ReadNameFromOffset(meb, Convert.ToInt16(customSize));
+                        }
+                    }
+                    else
+                    {
+                        ReadNameFromOffset(args[0], Convert.ToInt16(customSize));
+                    }
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Command line arguments were null (open by dragging a folder or file onto the exe)");
+                Console.ReadLine();
+            }
+        }
+    }
+}
